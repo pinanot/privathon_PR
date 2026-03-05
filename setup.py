886690453626795312c0bdf6bec09aaeb20ee1cd
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-03-06 00:03:24 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="privathon",
+    version="0.0.1",
+    description = "private scoped python",
+    py_modules=["privation"],
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    keywords=["privathion", "private", "scoped", "python"]
+)
